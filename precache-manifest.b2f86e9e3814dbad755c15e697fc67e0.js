103dea526f5da6f9c713b059c08d09d4c8b5d999
kevinjohnkiely <kevinjohnkiely1@hotmail.com>
2020-06-10 10:44:24 +0100

    Updates

---
+++ precache-manifest.b2f86e9e3814dbad755c15e697fc67e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332413f4d137b06b4a11a6a9feb1aa0e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7d7b9bfaa920a5276975",
+    "url": "/robofriends/static/css/main.5dff0a3a.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7b9bfaa920a5276975",
+    "url": "/robofriends/static/js/main.80f06dd8.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
